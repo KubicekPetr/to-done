0db64cb8a9cde6c9b06c7e423fe4d9e6ab930380
KubicekPetr <kubicekpetr@outlook.com>
2021-03-24 23:40:42 +0100

    allow env in api and import oak in api and start server

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,1 +1,9 @@
-console.log('Deno from docker is running');
+import { Application } from '../deps.ts';
+
+const app = new Application();
+const URL = Deno.env.get('URL') || 'http://localhost';
+const PORT = Deno.env.get('PORT') || 3001;
+
+app.addEventListener("listen", () => {
+    console.log(`Server is listening at ${URL}:${PORT}`);
+})

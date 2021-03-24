f5694bf0f7ae3f3ca838fe16ee9d6ad7bc73b9e4
KubicekPetr <kubicekpetr@outlook.com>
2021-03-24 23:51:08 +0100

    import router, allow-net, make app listen on port 3001 on root route

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,20 @@
-import { Application } from '../deps.ts';
+import { Application, Router } from "../deps.ts";
 
 const app = new Application();
-const URL = Deno.env.get('URL') || 'http://localhost';
-const PORT = Deno.env.get('PORT') || 3001;
+
+const URL = Deno.env.get("URL") || "http://localhost";
+const PORT = +(Deno.env.get("PORT") || 3001);
+
+const router = new Router();
+
+router.get("/", (context: any) => {
+  context.response.body = "Hello from Oak";
+});
+
+app.use(router.routes());
 
 app.addEventListener("listen", () => {
-    console.log(`Server is listening at ${URL}:${PORT}`);
-})
+  console.log(`Server is listening at ${URL}:${PORT}`);
+});
+
+await app.listen({ port: PORT });

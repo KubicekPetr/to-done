eab601d84d0f95bd3327efc2a14e3ad8d9514b19
KubicekPetr <kubicekpetr@outlook.com>
2021-03-27 17:44:51 +0100

    refactoring to singleton pattern

--- api/src/helpers/mongodb.ts
+++ api/src/helpers/mongodb.ts
@@ -2,21 +2,34 @@ import { Bson, MongoClient } from "../../deps.ts";
 
 class MongoDatabase {
     public client: MongoClient;
+    private MONGO_URL: string;
+    private DB_NAME: string;
+    private static instance: MongoDatabase;
 
-    constructor(private dbName: string, private url: string) {
-        console.log('Connecting to mongo on ', url);
-        // this.dbName = dbName;
-        // this.url = url;
+    constructor() {
+        this.MONGO_URL = Deno.env.get("MONGO_URL") || "mongodb://localhost:27017";
+        this.DB_NAME = Deno.env.get("DB_NAME") || "todos";
         this.client = {} as MongoClient;
     }
 
     async connect() {
         this.client = new MongoClient();
-        await this.client.connect(this.url);
+        await this.client.connect(this.MONGO_URL);
     }
 
     get getDatabase() {
-        return this.client.database(this.dbName);
+        return this.client.database(this.DB_NAME);
+    }
+
+    static async getInstance(): Promise<MongoDatabase> {
+        if (!MongoDatabase.instance) {
+            MongoDatabase.instance = new MongoDatabase();
+        }
+
+        await MongoDatabase.instance.connect();
+        console.log('Connecting to mongo on ', MongoDatabase.instance.MONGO_URL);
+
+        return MongoDatabase.instance;
     }
 }
 
--- api/src/main.ts
+++ api/src/main.ts
@@ -4,15 +4,15 @@ import MongoDatabase from './helpers/mongodb.ts';
 
 const URL = Deno.env.get("URL") || "http://localhost";
 const PORT = +(Deno.env.get("PORT") || 3001);
-const MONGO_URL = Deno.env.get("MONGO_URL") || "mongodb://localhost:27017";
 
-const mongoDB = new MongoDatabase('todos', MONGO_URL);
-await mongoDB.connect();
+const mongoInstance = await MongoDatabase.getInstance();
 
-const result = await mongoDB.getDatabase.collection('todo').insertOne({
-  title: 'MyFirstTodo',
-  description: 'Save my first todo in mongo',
+const result = await mongoInstance.getDatabase.collection('todo').insertOne({
+  title: 'MySecondTodo',
+  description: 'Save my second todo in mongo',
 });
+
+console.log(result)
 
 const app = new Application();
 initRouters(app);

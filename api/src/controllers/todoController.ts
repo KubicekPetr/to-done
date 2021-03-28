36eb78bba5f56b0db2d01536644194b865d6c5a2
KubicekPetr <kubicekpetr@outlook.com>
2021-03-28 16:10:09 +0200

    get the query parameter from url and get one todo at get endpoint

--- api/src/controllers/todoController.ts
+++ api/src/controllers/todoController.ts
@@ -1,3 +1,5 @@
+import { Bson } from '../../deps.ts';
+
 import MongoDatabase from "../helpers/mongodb.ts";
 import { validateTodo } from "../utils/validation.ts";
 import { ITodo } from "../models/todoModel.ts";
@@ -6,45 +8,41 @@ const db = (await MongoDatabase.getInstance()).getDatabase;
 const todoCollection = db.collection("todo");
 
 export const getAll = async (context: any) => {
-  console.log('Getting all todos');
+  console.log("Getting all todos");
   let response: Object;
   try {
     const data = await todoCollection.find().toArray();
     response = {
       success: true,
       length: data.length,
-      data
+      data,
     };
     context.response.body = JSON.stringify(response);
   } catch (error) {
     response = {
       success: false,
       length: 0,
-      error
+      error,
     };
   }
 };
 
-export const get = (context: any) => {
-  console.log("Getting a todo");
+export const get = async (context: any) => {
+  const id: string = context.params.id;
+  console.log(`Getting the todo with id ${id}`);
+  let response: Object;
   try {
-    const todo = {
-      title: "My Todo",
-      description: "Todo description",
-      createdAt: new Date(),
-    };
-    const response = {
+    const data = await todoCollection.findOne({ _id: new Bson.ObjectId(id) });
+    response = {
       success: true,
-      todo,
+      data,
     };
     context.response.body = JSON.stringify(response);
   } catch (error) {
-    const response = {
+    response = {
       success: false,
       error,
     };
-    context.response.status = 500;
-    context.response.body = JSON.stringify(response);
   }
 };
 

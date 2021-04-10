4b73e37ae079c9097ca2fcb74eb1dfe6729e441b
kubicekpetr <kubicekpetr@outlook.com>
2021-04-10 23:40:39 +0200

    update to higher version in docker-compose to use init feature for forwading os signals to docker service (deno api) and listen for signals in deno app

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,6 +2,17 @@ import { Application } from "../deps.ts";
 import initRouters from "./routers/index.ts";
 import notFound from "./middlewares/notFound.ts";
 
+async function onSignal(signal: number, callback: () => void) {
+  for await (const _ of Deno.signal(signal)) {
+    callback();
+  }
+}
+
+onSignal(Deno.Signal.SIGINT, () => { console.log("SIGINT") });
+onSignal(Deno.Signal.SIGTERM, () => { console.log("SIGTERM") });
+onSignal(Deno.Signal.SIGQUIT, () => { console.log("SIGQUIT") });
+
+
 const URL = Deno.env.get("URL") || "http://localhost";
 const PORT = +(Deno.env.get("PORT") || 3001);
 

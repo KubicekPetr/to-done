686de1eddbd8515fcd8cd7bbac734192d4029e85
kubicekpetr <kubicekpetr@outlook.com>
2021-04-15 23:12:57 +0200

    creating a navbar with logo and search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,22 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      React from docker is running
-      <span uk-icon="icon: check"></span>
-    </div>
+    <nav className='uk-navbar-container' data-uk-navbar>
+      <div className='uk-navbar-left'>
+        <a href='#!' className='uk-navbar-item'>To-Done</a>
+      </div>
+
+      <div className='uk-navbar-right'>
+        <div>
+          <a href='#!' className='uk-navbar-toggle' data-uk-search-icon></a>
+          <div className='uk-drop' data-uk-drop='mode: click; pos: left-center; offset: 0'>
+            <form className='uk-search-navbar uk-width-1-1'>
+              <input type="search" className="uk-search-input" placeholder="search..." autoFocus />
+            </form>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 }
 

8fc0e15b0d3a8bcf932937cc3b2731ce95260b03
kubicekpetr <kubicekpetr@outlook.com>
2021-04-17 23:58:19 +0200

    create a component for floating action button

---
+++ frontend/src/components/floating-action.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function FloatingAction() {
+    return (
+        <div style={{
+            position: "fixed",
+            right: 23,
+            bottom: 23,
+            zIndex: 999,
+        }}>
+            <a className="uk-icon-button" href="#!" uk-icon="plus" style={{
+                width: 56,
+                height: 56,
+                color: "white",
+                background: "#111",
+                zIndex: 999,
+                boxShadow: "3px 3px 2px 1px rgba(0, 0, 0, 0.2)",
+            }}></a>
+        </div>
+    )
+}
+
+export default FloatingAction;
